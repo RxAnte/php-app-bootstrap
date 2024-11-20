ef0514cb2465b9f68f097df0ccfef783ea61578f
TJ Draper <tdraper@rxante.com>
2024-11-20 10:15:44 -0600

    Add ApplyRoutesEventTest

---
+++ src/Http/ApplyRoutesEventTest.php
@@ -0,0 +1,196 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RxAnte\AppBootstrap\Http;
+
+use RxAnte\AppBootstrap\Http\TestSupport\ApplyRoutesTestSupport;
+use Slim\Exception\HttpMethodNotAllowedException;
+use Slim\Exception\HttpNotFoundException;
+use Slim\Factory\AppFactory;
+use Throwable;
+
+use function expect;
+use function test;
+use function uses;
+
+uses()->group('Bootstrap', 'ApplyRoutesEvent');
+
+test(
+    'Apply routes event routing methods are available',
+    function (): void {
+        $app = AppFactory::create();
+
+        /** @phpstan-ignore-next-line */
+        $applyRoutesEvent = new ApplyRoutesEvent($app);
+
+        $testSupport = new ApplyRoutesTestSupport();
+
+        $testSupport->setRoutes($applyRoutesEvent);
+
+        $fooGetResponse = $app->handle($testSupport->createRequest(
+            '/foo-get',
+            'GET',
+        ));
+        expect($fooGetResponse->getBody()->getContents())
+            ->toBe('get-foo');
+
+        $fooPostResponse = $app->handle($testSupport->createRequest(
+            '/foo-post',
+            'POST',
+        ));
+        expect($fooPostResponse->getBody()->getContents())
+            ->toBe('post-foo');
+
+        $fooPutResponse = $app->handle($testSupport->createRequest(
+            '/foo-put',
+            'PUT',
+        ));
+        expect($fooPutResponse->getBody()->getContents())
+            ->toBe('put-foo');
+
+        $fooPatchResponse = $app->handle($testSupport->createRequest(
+            '/foo-patch',
+            'PATCH',
+        ));
+        expect($fooPatchResponse->getBody()->getContents())
+            ->toBe('patch-foo');
+
+        $fooDeleteResponse = $app->handle($testSupport->createRequest(
+            '/foo-delete',
+            'DELETE',
+        ));
+        expect($fooDeleteResponse->getBody()->getContents())
+            ->toBe('delete-foo');
+
+        $fooOptionsResponse = $app->handle($testSupport->createRequest(
+            '/foo-options',
+            'OPTIONS',
+        ));
+        expect($fooOptionsResponse->getBody()->getContents())
+            ->toBe('options-foo');
+
+        // Any
+        $fooAnyResponseGet = $app->handle($testSupport->createRequest(
+            '/foo-any',
+            'GET',
+        ));
+        expect($fooAnyResponseGet->getBody()->getContents())
+            ->toBe('any-foo');
+
+        $fooAnyResponsePost = $app->handle($testSupport->createRequest(
+            '/foo-any',
+            'POST',
+        ));
+        expect($fooAnyResponsePost->getBody()->getContents())
+            ->toBe('any-foo');
+
+        $fooAnyResponsePut = $app->handle($testSupport->createRequest(
+            '/foo-any',
+            'PUT',
+        ));
+        expect($fooAnyResponsePut->getBody()->getContents())
+            ->toBe('any-foo');
+
+        $fooAnyResponsePatch = $app->handle($testSupport->createRequest(
+            '/foo-any',
+            'PATCH',
+        ));
+        expect($fooAnyResponsePatch->getBody()->getContents())
+            ->toBe('any-foo');
+
+        $fooAnyResponseDelete = $app->handle($testSupport->createRequest(
+            '/foo-any',
+            'DELETE',
+        ));
+        expect($fooAnyResponseDelete->getBody()->getContents())
+            ->toBe('any-foo');
+
+        $fooAnyResponseOptions = $app->handle($testSupport->createRequest(
+            '/foo-any',
+            'OPTIONS',
+        ));
+        expect($fooAnyResponseOptions->getBody()->getContents())
+            ->toBe('any-foo');
+
+        // Map
+        $fooMapResponseGet = $app->handle($testSupport->createRequest(
+            '/foo-map',
+            'GET',
+        ));
+        expect($fooMapResponseGet->getBody()->getContents())
+            ->toBe('map-foo');
+
+        $fooMapResponseGet = $app->handle($testSupport->createRequest(
+            '/foo-map',
+            'POST',
+        ));
+        expect($fooMapResponseGet->getBody()->getContents())
+            ->toBe('map-foo');
+
+        $mapInvalidVerbException = null;
+
+        try {
+            $app->handle($testSupport->createRequest(
+                '/foo-map',
+                'PUT',
+            ));
+        } catch (Throwable $e) {
+            $mapInvalidVerbException = $e;
+        }
+
+        expect($mapInvalidVerbException)
+            ->toBeInstanceOf(HttpMethodNotAllowedException::class);
+
+        // Group
+        $fooGroupResponse = $app->handle($testSupport->createRequest(
+            '/foo-group/foo-group-route-test',
+            'GET',
+        ));
+        expect($fooGroupResponse->getBody()->getContents())
+            ->toBe('group-route-test-foo');
+
+        // Redirect
+        $fooRedirectResponse = $app->handle($testSupport->createRequest(
+            '/foo-redirect',
+            'GET',
+        ));
+        expect($fooRedirectResponse->getHeader('Location')[0])
+            ->toBe('/redirect-foo')
+            ->and($fooRedirectResponse->getStatusCode())
+            ->toBe(123);
+
+        // Invalid route
+        $notFoundExceptionInvalidRoute = null;
+
+        try {
+            $app->handle($testSupport->createRequest(
+                '/foo-bar/bad-route',
+                'GET',
+            ));
+        } catch (Throwable $e) {
+            $notFoundExceptionInvalidRoute = $e;
+        }
+
+        expect($notFoundExceptionInvalidRoute)
+            ->toBeInstanceOf(HttpNotFoundException::class);
+
+        $invalidVerbException = null;
+
+        try {
+            $app->handle($testSupport->createRequest(
+                '/foo-get',
+                'POST',
+            ));
+        } catch (Throwable $e) {
+            $invalidVerbException = $e;
+        }
+
+        expect($invalidVerbException)
+            ->toBeInstanceOf(HttpMethodNotAllowedException::class);
+
+        $app->setBasePath('/foo/bar');
+        expect($applyRoutesEvent->getBasePath())
+            ->toBe('/foo/bar');
+    },
+);
---
+++ src/Http/TestSupport/ApplyRoutesTestSupport.php
@@ -0,0 +1,129 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RxAnte\AppBootstrap\Http\TestSupport;
+
+use Mockery;
+use Mockery\MockInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\StreamInterface;
+use RxAnte\AppBootstrap\Http\ApplyRoutesEvent;
+use Slim\Psr7\Headers;
+use Slim\Psr7\Request;
+use Slim\Psr7\Uri;
+use Slim\Routing\RouteCollectorProxy;
+
+use function assert;
+
+readonly class ApplyRoutesTestSupport
+{
+    public function setRoutes(ApplyRoutesEvent $applyRoutesEvent): void
+    {
+        $applyRoutesEvent->get('/foo-get', function () {
+            return $this->createResponse('get-foo');
+        });
+
+        $applyRoutesEvent->post('/foo-post', function () {
+            return $this->createResponse('post-foo');
+        });
+
+        $applyRoutesEvent->put('/foo-put', function () {
+            return $this->createResponse('put-foo');
+        });
+
+        $applyRoutesEvent->patch('/foo-patch', function () {
+            return $this->createResponse('patch-foo');
+        });
+
+        $applyRoutesEvent->delete('/foo-delete', function () {
+            return $this->createResponse('delete-foo');
+        });
+
+        $applyRoutesEvent->options('/foo-options', function () {
+            return $this->createResponse('options-foo');
+        });
+
+        $applyRoutesEvent->any('/foo-any', function () {
+            return $this->createResponse('any-foo');
+        });
+
+        $applyRoutesEvent->map(
+            ['GET', 'POST'],
+            '/foo-map',
+            function () {
+                return $this->createResponse('map-foo');
+            },
+        );
+
+        $applyRoutesEvent->group(
+            '/foo-group',
+            function (RouteCollectorProxy $routeCollector): void {
+                $routeCollector->get(
+                    '/foo-group-route-test',
+                    function () {
+                        return $this->createResponse(
+                            'group-route-test-foo',
+                        );
+                    },
+                );
+            },
+        );
+
+        $applyRoutesEvent->redirect(
+            '/foo-redirect',
+            '/redirect-foo',
+            123,
+        );
+    }
+
+    public function createRequest(string $path, string $method): Request
+    {
+        $uri = new Uri(
+            'https',
+            'testing.dev',
+            443,
+            $path,
+            '',
+            '',
+            '',
+            '',
+        );
+
+        $headers = new Headers();
+
+        $stream = Mockery::mock(StreamInterface::class);
+
+        assert($stream instanceof StreamInterface);
+
+        return new Request(
+            $method,
+            $uri,
+            $headers,
+            [],
+            [],
+            $stream,
+            [],
+        );
+    }
+
+    private function createResponse(string $bodyText): ResponseInterface
+    {
+        $body = Mockery::mock(StreamInterface::class);
+
+        assert($body instanceof MockInterface);
+
+        /** @phpstan-ignore-next-line */
+        $body->shouldReceive('getContents')
+            ->andReturn($bodyText);
+
+        $response = Mockery::mock(ResponseInterface::class);
+
+        /** @phpstan-ignore-next-line */
+        $response->shouldReceive('getBody')->andReturn($body);
+
+        assert($response instanceof ResponseInterface);
+
+        return $response;
+    }
+}
